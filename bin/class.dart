7e4c1a6b39c1333032cf0b9845fa7c3cd8ea74b0
Khoerodin <heykhoerodin@gmail.com>
2023-12-16 17:55:34 +0700

    Field

--- bin/class.dart
+++ bin/class.dart
@@ -1,9 +1,18 @@
-class Person {}
+class Person {
+  String name = 'Guest';
+  String? address;
+  final String country = 'Indonesia';
+}
 
 void main() {
   var person1 = Person();
   var person2 = Person();
 
-  print(person1);
+  person1.name = 'Khoerodin';
+  person1.address = 'Ciamis';
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
+
   print(person2);
 }

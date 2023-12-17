0bdb6e573354fbdb825cb6d05322a7cd49eafa2c
Khoerodin <heykhoerodin@gmail.com>
2023-12-17 14:56:40 +0700

    Method Overriding

---
+++ bin/method_overrding.dart
@@ -0,0 +1,23 @@
+class Manager {
+  String? name;
+
+  void sayHello(String name) {
+    print('Hello $name!, I am a Manager, my name is ${this.name}');
+  }
+}
+
+class VicePresident extends Manager {
+  void sayHello(String name) {
+    print('Hello $name!, I am a Vice President, my name is ${this.name}');
+  }
+}
+
+void main() {
+  Manager manager = Manager();
+  manager.name = 'Budi';
+  manager.sayHello('Eko');
+
+  VicePresident vicePresident = VicePresident();
+  vicePresident.name = 'Agus';
+  vicePresident.sayHello('Anwar');
+}

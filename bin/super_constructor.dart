fe59a0a0065e174414736c65b496060e079e6f67
Khoerodin <heykhoerodin@gmail.com>
2023-12-17 17:30:46 +0700

    Super Constructor

---
+++ bin/super_constructor.dart
@@ -0,0 +1,14 @@
+class Manager {
+  String? name;
+  Manager(this.name);
+}
+
+class VicePresident extends Manager {
+  VicePresident(String name) : super(name) {
+    print('Create new VicePresident');
+  }
+}
+
+void main() {
+  VicePresident('Andi');
+}

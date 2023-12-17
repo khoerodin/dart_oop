85fa363c7a4bcbe53f5575bafff77b0dbe8486e7
Khoerodin <heykhoerodin@gmail.com>
2023-12-17 15:07:23 +0700

    Field Overriding

---
+++ bin/field_overriding.dart
@@ -0,0 +1,20 @@
+class Person {
+  String name = 'Bagus';
+
+  void sayHello(String name) {
+    print('Hi $name, my name is ${this.name}.');
+  }
+}
+
+class OtherPerson extends Person {
+  @override
+  String name = 'Imam';
+}
+
+void main() {
+  Person orang = Person();
+  orang.sayHello('Andi');
+
+  Person orangLain = OtherPerson();
+  orangLain.sayHello('Andi');
+}

33dda39d42ee861311f2f8e7172d60afe3f35c85
Khoerodin <heykhoerodin@gmail.com>
2023-12-19 06:49:39 +0700

    Equals Operator

--- bin/data/category.dart
+++ bin/data/category.dart
@@ -3,4 +3,18 @@ class Category {
   String name;
 
   Category(this.id, this.name);
+
+  bool operator ==(Object other) {
+    if (other is Category) {
+      if (id != other.id) {
+        return false;
+      } else if (name != other.name) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
 }
---
+++ bin/equals.dart
@@ -0,0 +1,9 @@
+import 'data/category.dart';
+
+void main() {
+  var category1 = Category('1', 'Buku');
+  var category2 = Category('1', 'Buku');
+
+  print(category1 == category1);
+  print(category1 == category2);
+}
